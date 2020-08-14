a99af56d771fb6a0ba330570934da74ecdd763a6
teabolt <tomasbaltrunas0@gmail.com>
2020-08-14 23:07:48 +0100

    Add environment files, request logging middleware

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
+
+require('./config/env');
 
 const routes = require('./routes/index.js');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// Middleware
+app.use(morgan('combined'));
+
+// Register routes
 app.use(routes);
 
 app.listen(PORT, () => {
