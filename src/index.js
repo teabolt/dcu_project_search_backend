ad060becc7f9b232d451b87b315327fbff6c5f1b
teabolt <tomasbaltrunas0@gmail.com>
2020-09-05 21:27:21 +0100

    Add CORS middleware with allowed hosts

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-console */
+const cors = require('cors');
 const express = require('express');
 const morgan = require('morgan');
 
@@ -11,11 +12,21 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Middleware
+
+// Logging
 app.use(morgan('combined'));
+
+// CORS
+const corsOptions = {
+  origin: process.env.FE_HOST,
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 // Register routes
 app.use(routes);
 
+// Start server
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`);
 });
