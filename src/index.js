2714b2effcfc0d0e90f6b250b313d221331bbdab
teabolt <tomasbaltrunas0@gmail.com>
2020-09-27 21:42:52 +0100

    Serve HTTPS

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
+/* eslint-disable no-sync */
 /* eslint-disable no-console */
+
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+
 const cors = require('cors');
 const express = require('express');
 const morgan = require('morgan');
@@ -27,7 +33,26 @@ app.use(cors(corsOptions));
 // Register routes
 app.use(routes);
 
+const IS_HTTPS = process.env.NODE_ENV === 'production';
+
+let server;
+if (IS_HTTPS) {
+  const credentials = {
+    key: fs.readFileSync(process.env.SSL_KEY),
+    cert: fs.readFileSync(process.env.SSL_CERT),
+  };
+  server = https.createServer(credentials, app);
+} else {
+  server = http.createServer(app);
+}
+
 // Start server
-app.listen(PORT, HOST, () => {
-  console.log(`App listening at http://${HOST}:${PORT}`);
-});
+if (server) {
+  server.listen({ port: PORT, host: HOST }, () => {
+    console.log(
+      `App listening at http${IS_HTTPS ? 's' : ''}://${HOST}:${PORT}`
+    );
+  });
+} else {
+  console.error(`Server is not created: ${server}`);
+}
